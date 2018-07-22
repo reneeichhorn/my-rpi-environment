2a1435d0aaf1a2dfb5ba7eeec3c861f89ffe76d8
Rene Eichhorn <rene.eichhorn1@gmail.com>
2018-07-22 14:16:09 +0200

    initial ecovacs controller

---
+++ ecovacs-controller/build.sh
@@ -0,0 +1,1 @@
+docker build . -t ecovacs-controller --network=host
---
+++ ecovacs-controller/service.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import os
+from sucks import *
+
+class BumperVacBot(VacBot):
+    def __init__(self, server_address):
+        self.server_address = server_address
+        vacuum = { 'did':'none','class':'none' }
+        super().__init__('sucks', 'ecouser.net', '', '', vacuum, '')
+    def connect_and_wait_until_ready(self):
+        logging.info('connecting')
+        self.xmpp.connect(self.server_address)
+        self.xmpp.process()
+        self.xmpp.wait_until_ready()
+
+logging.basicConfig(level=logging.DEBUG, format='%(levelname)-8s %(message)s')
+server_address = (os.getenv('ECOVACS_IP', '192.168.178.24'), 5223)
+
+# Initialize
+vacbot = BumperVacBot(server_address)
+
+# Connect
+vacbot.connect_and_wait_until_ready()
+
+# Send a command
+vacbot.run(Clean())
